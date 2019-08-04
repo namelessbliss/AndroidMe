a5f661b7be0a80b9df3e3a3fd6a666bcbc495316
namelessbliss <anonimo2911.2012@gmail.com>
2019-08-04 16:51:37 -0500

    Onclick para cada fragment y guarda el estado de la lista y el index  al rotar

--- app/src/main/java/com/app/nb/androidme/fragments/BodyPartFragment.java
+++ app/src/main/java/com/app/nb/androidme/fragments/BodyPartFragment.java
@@ -11,10 +11,16 @@ import android.widget.ImageView;
 
 import com.app.nb.androidme.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BodyPartFragment extends Fragment {
 
+    // constantes para almacenar la info del estado de la lista de iamges y su index (key:value)
+    public static final String IMAGE_ID_LIST = "image_ids";
+    public static final String LIST_INDEX = "List_index";
+
+    // Variables para almacenar la lista de iamgenes y el index de la imagen a mostrar
     private List<Integer> mImagesIds;
     private int mListImageIndex;
 
@@ -24,12 +30,28 @@ public class BodyPartFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        // Carga el estado guardado (la lista de imagenes y el index) si lo hay
+        mImagesIds = savedInstanceState.getIntegerArrayList(IMAGE_ID_LIST);
+        mListImageIndex = savedInstanceState.getInt(LIST_INDEX);
+
         // Inflate the layout
         View view = inflater.inflate(R.layout.fragment_body_part, container, false);
 
-        ImageView imageView = view.findViewById(R.id.body_part_image_view);
+        final ImageView imageView = view.findViewById(R.id.body_part_image_view);
 
         imageView.setImageResource(mImagesIds.get(mListImageIndex));
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mListImageIndex < mImagesIds.size() - 1)
+                    mListImageIndex++;
+                else
+                    mListImageIndex = 0;
+                imageView.setImageResource(mImagesIds.get(mListImageIndex));
+            }
+        });
 
         return view;
     }
@@ -40,5 +62,14 @@ public class BodyPartFragment extends Fragment {
 
     public void setmListImageIndex(int mListImageIndex) {
         this.mListImageIndex = mListImageIndex;
+    }
+
+    /**
+     * Guardar el estado actual del fragment
+     */
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putIntegerArrayList(IMAGE_ID_LIST, (ArrayList<Integer>) mImagesIds);
+        outState.putInt(LIST_INDEX, mListImageIndex);
     }
 }
